f26f23b9bd3e4466559e53baeb5a042f798c45ff
Muhammad <samahkinets@gmail.com>
2025-10-07 00:30:57 +0300

    multilingual test

--- frontend/src/shared/utils/i18n/i18n-client.ts
+++ frontend/src/shared/utils/i18n/i18n-client.ts
@@ -2,16 +2,13 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-
-
 import treeEN from './messages/en/tree.json'
 import treeRU from './messages/ru/tree.json'
-import i18nConfig from '../../../../i18nConfig'
 
 i18n.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
-		fallbackLng: i18nConfig.defaultLocale,
+		fallbackLng: 'en',
 		detection: {
 			order: ['cookie', 'htmlTag', 'navigator'],
 			lookupCookie: 'NEXT_LOCALE',  
