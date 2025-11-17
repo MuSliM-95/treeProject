784e7535ceec2a22cc5c26fac7783a011d51db5f
Muhammad <samahkinets@gmail.com>
2025-11-18 00:12:19 +0300

    Added a leading / to the API endpoints.

--- frontend/src/features/auth/services/auth.service.ts
+++ frontend/src/features/auth/services/auth.service.ts
@@ -7,39 +7,39 @@ import { ResponseExists, ResponseLoginData, ResponseRegisterData } from '../type
 class AuthService {
 	public async register(body: TypeRegisterSchema) {
 
-		const response = await api.post<ResponseRegisterData>('api/auth/register', body)
+		const response = await api.post<ResponseRegisterData>('/api/auth/register', body)
 
 		return response
 	}
 
 	public async login(body: TypeLoginSchema) {
-		const response = await api.post<ResponseLoginData>('api/auth/login', body)
+		const response = await api.post<ResponseLoginData>('/api/auth/login', body)
 
 		return response
 	}
 
 	public async oauthByProvider(provider: 'google' | 'yandex', lang: string) {
 		const response = await api.get<{ url: string }>(
-			`api/oauth/connect/${provider}`
+			`/api/oauth/connect/${provider}`
 		)
 
 		return response
 	}
 
 	public async fetchExistsInfo(token: string, lang: string) {
-		const response = await api.get<ResponseExists>(`api/oauth/exists-info?token=${token}`)
+		const response = await api.get<ResponseExists>(`/api/oauth/exists-info?token=${token}`)
 
 		return response
 	}
 
 	public async updateEmail(body: TypeChangeEmailSchema) {
-		const response = await api.post(`api/auth/update-email`, body)
+		const response = await api.post(`/api/auth/update-email`, body)
 
 		return response
 	}
 
 	public async logout() {
-		const response = await api.post('api/auth/logout')
+		const response = await api.post('/api/auth/logout')
 
 		return response
 	}
