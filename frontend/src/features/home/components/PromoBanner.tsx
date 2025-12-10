1087b2f72aa1b66a1f6b940f972947f69b3ab5ca
Muhammad <samahkinets@gmail.com>
2025-12-11 02:57:07 +0300

    Ad banner test.

--- frontend/src/features/home/components/PromoBanner.tsx
+++ frontend/src/features/home/components/PromoBanner.tsx
@@ -6,6 +6,11 @@ export const PromoBanner: React.FC = () => {
 	return (
 		<section className='relative mx-auto mt-10 w-full max-w-7xl overflow-hidden rounded-xl bg-gradient-to-r from-[#6a82fa] to-[#8abffb] p-4 text-white shadow-lg'>
 			<iframe
+				src='https://www.effectivegatecpm.com/cbkhrf92nc?key=d2aeaecfd7ba35addb08fdfe759aa8cc'
+				height='250'
+				width='300'
+			/>
+			<iframe
 				src='https://www.effectivegatecpm.com/cbkhrf92nc?key=ed89702e00a3b75ad21707825c5530cf'
 				height='90'
 				width='728'
