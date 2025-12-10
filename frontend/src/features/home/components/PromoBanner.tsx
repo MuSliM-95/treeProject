6d0d17765b27c431632988eb419af5fdb47e115a
Muhammad <samahkinets@gmail.com>
2025-12-10 20:27:32 +0300

    Ad banner test.

--- frontend/src/features/home/components/PromoBanner.tsx
+++ frontend/src/features/home/components/PromoBanner.tsx
@@ -1,35 +1,20 @@
-
-import React from 'react'
-import Link from 'next/link'
-
 import { TFunction } from 'i18next'
+import Link from 'next/link'
+import React from 'react'
 
 interface IPromoBanner {
-  t: TFunction
+	t: TFunction
 }
 
-export const PromoBanner: React.FC<IPromoBanner> = ({t}) => {
-
-  return (
-    <section className="relative w-full overflow-hidden text-white shadow-lg mx-auto mt-10 max-w-7xl bg-gradient-to-r from-[#6a82fa] to-[#8abffb] p-4 rounded-xl">
-      <div className="mx-auto flex max-w-5xl flex-col items-center gap-6 px-6 py-8 text-center md:flex-row md:gap-12 md:text-left">
-        
-        {/* Текст */}
-        <div className="flex-1">
-          <h2 className="text-3xl font-bold md:text-4xl">Split. {t('homepage.title')}</h2>
-          <p className="mt-4 text-lg opacity-90">{t('homepage.subtitle')}</p>
-
-          <div className="mt-6 flex justify-center md:justify-start">
-            <Link
-              href="https://split.tg/?ref=UQBUhH6P4oSxb-4_loEwEvOQUsFmXXr_3goPcNyx1Mqsy7bj" // 🔗 тут вставь ссылку на своё приложение
-              className="rounded-full bg-white px-8 py-3 text-blue-600 font-medium shadow hover:bg-gray-100 transition"
-              target='_blank'
-            >
-              {t('homepage.learnMore')}
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+export const PromoBanner: React.FC<IPromoBanner> = ({ t }) => {
+	return (
+		<section className='relative mx-auto mt-10 w-full max-w-7xl overflow-hidden rounded-xl bg-gradient-to-r from-[#6a82fa] to-[#8abffb] p-4 text-white shadow-lg'>
+			<iframe
+				src='https://www.highperformanceformat.com/79db065236639e644fcb02bd3558a8f0/iframe.html'
+				width='728'
+				height='90'
+				style={{ border: 'none', overflow: 'hidden' }}
+			></iframe>
+		</section>
+	)
 }
