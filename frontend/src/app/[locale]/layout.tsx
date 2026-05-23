98619e4ce110ba98fa3887c55357e6e269551d8a
Muhammad <samahkinets@gmail.com>
2026-05-23 05:55:07 +0300

    test

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -4,16 +4,14 @@ import { ReactNode } from 'react'
 
 import ConsentBanner from '@/shared/components/ui/ConsentBanner'
 import TranslationsProvider from '@/shared/providers/TranslationsProvider'
+import { IProps } from '@/shared/types/locale.type'
 import { createAlternates } from '@/shared/utils'
-
-import initTranslations from '../../shared/utils/i18n/i18n'
 import { CreateOpenGraph } from '@/shared/utils/seo/create.open.graph'
 import { createTwitterMeta } from '@/shared/utils/seo/create.twitter'
-import { IProps } from '@/shared/types/locale.type'
 
-export async function generateMetadata({
-	params
-}: IProps): Promise<Metadata> {
+import initTranslations from '../../shared/utils/i18n/i18n'
+
+export async function generateMetadata({ params }: IProps): Promise<Metadata> {
 	const { locale } = await params
 	const { t } = await initTranslations({
 		locale: locale,
@@ -27,24 +25,30 @@ export async function generateMetadata({
 		icons: {
 			icon: [
 				{
-					url: 'https://genealogyhub.ru/images/favicon.svg',
-					type: 'image/svg+xml'
-				}
-			],
-
-			apple: [
+					url: '/images/favicon-32x32.png',
+					sizes: '32x32',
+					type: 'image/png'
+				},
 				{
-					url: 'https://genealogyhub.ru/images/favicon.png',
-					sizes: '180x180',
+					url: '/images/favicon-16x16.png',
+					sizes: '16x16',
 					type: 'image/png'
 				}
-			]
+			],
+			apple: {
+				url: '/images/apple-touch-icon.png',
+				sizes: '180x180'
+			}
 		},
 		verification: {
 			google: 'K4NjTpnl-Yu4tWYOS-afGUtZA3PIIZM-AH30tKuJrzo',
 			yandex: '016d187c35064f23'
 		},
-		openGraph: CreateOpenGraph(t, createAlternates('/tree', locale).canonical, locale),
+		openGraph: CreateOpenGraph(
+			t,
+			createAlternates('/tree', locale).canonical,
+			locale
+		),
 		twitter: createTwitterMeta(t),
 		alternates: createAlternates('/tree', locale)
 	}
